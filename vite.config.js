8f15d2fdfeb177ca74dbbee221d00f9843580e5d
BlueBeagBB <gabrifelipegf@gmail.com>
2025-06-30 17:15:30 -0300

    configuracoes de deploy

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,10 @@ export default defineConfig({
       name: "markdown-loader",
       transform(code, id) {
         if (id.slice(-3) === ".md") {
-          // For .md files, get the raw content
           return `export default ${JSON.stringify(code)};`;
         }
       }
     }
   ],
+  base: "/curso-react/",
 })
